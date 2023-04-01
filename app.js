173c1c73a9c284aa2cbe06b8c2c907b00a4dc1a5
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 <getahunlast@gmail.com>
2023-04-01 23:29:11 +0300

    homepage update

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 
-
 window.onscroll=function(){
+    //variables to track the position of scrolling.
     var svalue=window.scrollY;
+    var scrollPosition=window.pageYOffset;
+
    var navBar= document.getElementById("myNav");
    var donButton=document.getElementById("Donete");
-   var chard=document.getElementById("statstic");
+   var banner=document.getElementById("banner");
+   var description=document.getElementById("description");
+
+   banner.style.backgroundPosition=`center ${-scrollPosition /5 }px`;
+
+   description.style.opacity=`${1-scrollPosition /300}`;
+
+   
     if(window.pageYOffset > myNav.offsetTop){
         navBar.classList.add("sticky");
          donButton.classList.add("doanteButton");
-         chard.classList.add("ShowCard");
     }
     else{
         navBar.classList.remove("sticky");
@@ -16,3 +24,11 @@ window.onscroll=function(){
     }
     
 };
+
+const rotateImage = document.querySelector('.logoImage');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.pageYOffset;
+  const angle = scrollTop * 10;
+  rotateImage.style.transform = `rotate(${angle}deg)`;
+});
