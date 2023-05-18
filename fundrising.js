a5d384d0c62566cdfdd2b60437f068c80e82ce27
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 <getahunlast@gmail.com>
2023-05-18 22:12:31 +0300

    connect pages

--- fundrising.js
+++ fundrising.js
@@ -1,6 +1,7 @@
 const progressBar = document.querySelector(".progress");
 const progressPersentage=document.querySelector(".percentage");
 const amountRaised = document.getElementById("amount");
+const thanksText=document.querySelector(".thanks");
 const goal = document.getElementById("goal");
 const donateForm = document.querySelector("form");
 const donationAmount = document.getElementById("donation-amount");
@@ -13,6 +14,7 @@ donateForm.addEventListener("submit", (e) => {
     let donation = parseInt(donationAmount.value);
     totalDonations += donation;
     amountRaised.innerText = totalDonations;
+    thanksText.innerHTML= "Thank You for donating" +" "+ donation+" "+ "Birr";
     goal.innerHTML=fundraisingGoal;
     let percentageRaised = (totalDonations / fundraisingGoal) * 100;
    
