af0aeb2f40dc239633ae39456f7ed7f08da4f573
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 <getahunlast@gmail.com>
2023-05-18 21:45:02 +0300

    add amount automaticaly

--- fundrising.js
+++ fundrising.js
@@ -1,21 +1,24 @@
 const progressBar = document.querySelector(".progress");
 const progressPersentage=document.querySelector(".percentage");
 const amountRaised = document.getElementById("amount");
+const goal = document.getElementById("goal");
 const donateForm = document.querySelector("form");
 const donationAmount = document.getElementById("donation-amount");
 
 let totalDonations = 0;
-const fundraisingGoal = 10000;
+let fundraisingGoal = 10000;
 
 donateForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let donation = parseInt(donationAmount.value);
     totalDonations += donation;
     amountRaised.innerText = totalDonations;
+    goal.innerHTML=fundraisingGoal;
     let percentageRaised = (totalDonations / fundraisingGoal) * 100;
    
     if(percentageRaised > 100){
         progressBar.style.width =100 + "%";
+        fundraisingGoal +=10000;
     }
     else{
         progressBar.style.width = percentageRaised + "%";
