b7604bfed61fc1afee2c627d68cccb3b05f0c0ce
getch <getahunlast@gmail.com>
2023-06-23 20:45:10 +0100

    nav bar active js edit

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,13 +1,22 @@
-let Navlink=document.querySelectorAll('.nav-list');
-for(let i=0 ;i <Navlink.length;i ++){
-    Navlink[i].addEventListener('click',function(){
-        for(let j = 0 ; j< Navlink.length; j++){
-            Navlink[j].classList.remove('active');
-        }
-        this.classList.add('active');
-    
-    })
-}
+// Get the current page URL
+var currentPage = window.location.href;
+
+// Get all the links in the menu
+var links = document.querySelectorAll('.menu .nav-list a');
+
+// Remove 'active' class from all links
+links.forEach(function(link) {
+  link.parentNode.classList.remove('active');
+});
+
+// Iterate through the links and add the 'active' class to the matching link
+links.forEach(function(link) {
+  if (link.href === currentPage) {
+    link.parentNode.classList.add('active');
+  }
+});
+
+
 window.onscroll=function(){
     //variables to track the position of scrolling.
     var svalue=window.scrollY;
